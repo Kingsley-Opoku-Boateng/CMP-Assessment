d75be050895169768f111c49df718730718e1e60
Kingsley-Opoku-Boateng <st20335961@outlook.cardiffmet.ac.uk>
2025-12-17 23:37:07 +0000

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
